f7ac262cd600418ed86f0fd03b4eed79555cfd54
Farhad <ifarhadfaghihi@gmail.com>
2019-03-23 13:43:50 +0430

    Updates app launcher icon Updated app name Updates app color scheme Renames MainActivity to HomeActivity

--- app/src/main/java/io/github/farhad/popcorn/ui/home/HomeActivity.kt
+++ app/src/main/java/io/github/farhad/popcorn/ui/home/HomeActivity.kt
@@ -1,12 +1,13 @@
-package io.github.farhad.popcorn
+package io.github.farhad.popcorn.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import io.github.farhad.popcorn.R
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
     }
 }
