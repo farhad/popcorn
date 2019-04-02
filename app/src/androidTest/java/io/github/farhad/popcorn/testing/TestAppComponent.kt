6a9edafa925ed2dfbe2c642e9a32054a64c3c9ca
Farhad <ifarhadfaghihi@gmail.com>
2019-04-02 18:57:28 +0430

    Fixes dagger issues after adding activity, fragment and viewmodel injection

--- app/src/androidTest/java/io/github/farhad/popcorn/testing/TestAppComponent.kt
+++ app/src/androidTest/java/io/github/farhad/popcorn/testing/TestAppComponent.kt
@@ -2,16 +2,19 @@ package io.github.farhad.popcorn.testing
 
 import dagger.BindsInstance
 import dagger.Component
+import dagger.android.AndroidInjectionModule
 import io.github.farhad.popcorn.PopcornApp
-import io.github.farhad.popcorn.di.AppComponent
-import io.github.farhad.popcorn.di.ApplicationScope
-import io.github.farhad.popcorn.di.DataSourceModule
-import io.github.farhad.popcorn.di.DbModule
+import io.github.farhad.popcorn.di.*
 import io.github.farhad.popcorn.remote.ApiServiceTest
 
 @ApplicationScope
 @Component(
     modules = [
+        AndroidInjectionModule::class,
+        ActivityModule::class,
+        FragmentModule::class,
+        AppModule::class,
+        ViewModelModule::class,
         DbModule::class,
         TestNetworkingModule::class,
         DataSourceModule::class]
