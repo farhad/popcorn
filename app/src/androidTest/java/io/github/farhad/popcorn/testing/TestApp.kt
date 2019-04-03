cd5589f369dcbb69bbe486b4244bc823f6f92f12
Farhad <ifarhadfaghihi@gmail.com>
2019-04-04 03:50:05 +0430

    Fixes dagger failing injection in ApiServiceTest -- Phew!

--- app/src/androidTest/java/io/github/farhad/popcorn/testing/TestApp.kt
+++ app/src/androidTest/java/io/github/farhad/popcorn/testing/TestApp.kt
@@ -1,9 +1,11 @@
 package io.github.farhad.popcorn.testing
 
 import io.github.farhad.popcorn.PopcornApp
-import io.github.farhad.popcorn.di.AppComponent
 
 class TestApp : PopcornApp() {
 
-    override var appComponent: AppComponent = DaggerTestAppComponent.builder().application(this).build()
+    override fun onCreate() {
+        super.onCreate()
+        appComponent = DaggerTestAppComponent.builder().application(this).build()
+    }
 }
