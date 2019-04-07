d4b89b75650682ba76ab1339c8cce68248a2829c
Farhad <ifarhadfaghihi@gmail.com>
2019-04-07 14:25:18 +0430

    wip - details ui

--- app/src/main/java/io/github/farhad/popcorn/ui/details/MovieDetailsFragment.kt
+++ app/src/main/java/io/github/farhad/popcorn/ui/details/MovieDetailsFragment.kt
@@ -1,8 +1,42 @@
 package io.github.farhad.popcorn.ui.details
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import io.github.farhad.popcorn.di.Injectable
+import io.github.farhad.popcorn.utils.ImageLoader
+import javax.inject.Inject
 
 class MovieDetailsFragment : Fragment(), Injectable {
+
+    @Inject
+    lateinit var viewModelFactory: ViewModelProvider.Factory
+
+    @Inject
+    lateinit var imageLoader: ImageLoader
+
+    private lateinit var viewModel : MovieDetailsViewModel
+
+    private lateinit var performerAdapter : MoviePerformersAdapter
+    private lateinit var rolesAdapter : MovieRolesAdapter
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    private fun initView() {
+
+    }
 
 }
